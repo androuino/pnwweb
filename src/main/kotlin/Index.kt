60870393f6a17c106d72a3bcfcaca5aab1e66616
shaolin30 <androuino.info@gmail.com>
2021-01-28 19:31:57 +0900

    first commit

---
+++ src/main/kotlin/Index.kt
@@ -0,0 +1,24 @@
+import com.intellisrc.web.Service
+import groovy.lang.MetaClass
+
+class Index : Service() {
+    override fun invokeMethod(name: String?, args: Any?): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getProperty(propertyName: String?): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun setProperty(propertyName: String?, newValue: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMetaClass(): MetaClass {
+        TODO("Not yet implemented")
+    }
+
+    override fun setMetaClass(metaClass: MetaClass?) {
+        TODO("Not yet implemented")
+    }
+}
